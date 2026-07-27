1189a5a090f6012e362904bf197a2cbef56f5f1f
salihdamar19 <salih1661@icloud.com>
2026-07-27 23:21:38 +0300

    README.md added

---
+++ file_organizer/README.md
@@ -0,0 +1,5 @@
+#GUIDE
+1. Download the file_organizer.py
+2. Move the file_organizer.py to the folder that you want to organize
+3. Right click and select open in terminal for windows
+4. Write "python file_organizer.py" and press Enter
--- file_organizer/file_organizer.py
+++ file_organizer/file_organizer.py
@@ -1,4 +1,5 @@
 #Auhor: Salih Damar
+#Project Name: File Organizer
 
 import os
 import shutil
@@ -15,14 +16,12 @@ def createFolder(extension):
         
 
 for file in files:
+    name = os.path.splitext(file)[0]
     extension = os.path.splitext(file)[1]
     extension = extension.replace('.','')
-    if extension == 'py':
+    if name == 'file_organizer':
         continue
     elif os.path.isdir(os.path.join(folder, extension)):
         shutil.move(os.path.join(folder, file), os.path.join(folder, extension))
     else:
         shutil.move(os.path.join(folder, file), createFolder(extension))
-    
-    
-
