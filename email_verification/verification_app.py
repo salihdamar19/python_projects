803c7e7ec46c8ad504b0521737a6cd9bf434e6ae
salihdamar19 <salih1661@icloud.com>
2026-08-05 16:01:39 +0300

    fixes

--- email_verification/verification_app.py
+++ email_verification/verification_app.py
@@ -1,18 +1,15 @@
 from flask import Flask, session,  render_template, request, redirect, url_for
-import smtplib
-from email.mime.text import MIMEText
-from email.mime.multipart import MIMEMultipart
 import os
 from dotenv import load_dotenv
 import random
-
+import resend
+import time
 
 app = Flask(__name__)
-app.secret_key = "gizli_sifre_12345"
+app.secret_key = os.getenv("SECRET_KEY")
 load_dotenv()
 
-email = os.getenv("EPOSTA_ADRESI")
-password = os.getenv("EPOSTA_SIFRE")
+resend.api_key = os.getenv("RESEND_API_KEY")
 
 
 
@@ -22,19 +19,25 @@ def anasayfa():
 
 @app.route("/uret", methods=["POST", "GET"])
 def uret():
+    if session.get("next_send") and session["next_send"] > time.time() :
+        return render_template("dogrula.html", message=f"Lufen yeni kod icin {int(session.get("expire") - 240 - time.time())}sn bekleyiniz.")
     kod = random.randint(100000,999999)
+    session["expire"] = time.time() + 300
+    session["next_send"] = time.time() + 60
     session["kod"] = kod
     alici = request.form["email"]
-    konu = "Dogrulama Kodu"
-    mesaj = MIMEMultipart()
-    mesaj["From"] = email
-    mesaj["To"] = alici
-    mesaj["Subject"] = konu
-    mesaj.attach(MIMEText(str(kod), "plain"))
-    with smtplib.SMTP("smtp.gmail.com", 587) as sunucu:
-        sunucu.starttls()
-        sunucu.login(email, password)
-        sunucu.send_message(mesaj)
+    session["email"] = alici
+    params = {
+        "from" : "onboarding@resend.dev",
+        "to": [alici],
+        "subject" : "Dogrulama Kodu",
+        "text": str(kod)
+    }
+    try:
+        resend.Emails.send(params)
+    except Exception as e:
+        return f"Mail gonderilemedi: {e}"
+        
     return redirect(url_for("dogrula"))
 
 @app.route("/arttir")
@@ -48,10 +51,19 @@ def arttir():
 def dogrula():
     if request.method == "POST":
         dogru_kod = str(session.get("kod"))
-        if request.form["kod"] == dogru_kod:
-            return "Dogru kod."
+        if time.time() > session["expire"]:
+            return render_template("dogrula.html", message="Kodun Suresi Doldu")
         else:
-            return "Yalnis kod"
+            if request.form["kod"] == dogru_kod:
+                session.pop("kod", None)
+                session.pop("expire", None)
+                session.pop("next_send", None)
+                return render_template("dogrula.html", message="Kod Dogru")
+            
+            elif request.form["kod"]: 
+                return render_template("dogrula.html", message="Kod Yanlis")
+            else:
+                render_template("dogrula.html", message="")
     return render_template("dogrula.html")
 
 if __name__ == "__main__":
