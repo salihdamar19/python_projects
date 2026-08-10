c8dcf216f2070ca067a6af7b957cec61ae6d15b1
salihdamar19 <salih1661@icloud.com>
2026-08-10 21:37:11 +0300

    style changes

--- sqLogin/app.py
+++ sqLogin/app.py
@@ -1,8 +1,9 @@
-import sqlite3
 import os
 from flask import Flask, render_template, session, redirect, url_for, request
 from dotenv import load_dotenv
 import create
+import random
+import time
 
 project = os.path.dirname(os.path.abspath(__file__))
 
@@ -13,7 +14,8 @@ brevo_key = os.getenv("BREVO_API_KEY")
 
 @app.route("/", methods=["POST", "GET"])
 def index():
-    return render_template("index.html")
+    message = session.pop("message", None)
+    return render_template("index.html", message=message)
 
 @app.route("/signin", methods=["POST"])
 def signin():
@@ -24,27 +26,34 @@ def signin():
     for user in users:
         if user[2] == email:
             if user[3] == password:
-                return render_template("index.html", message="Giris Basarili")
+                session["message"] = "Giris Basarili"
+                return redirect(url_for("index"))
 
-    return render_template("index.html", message="Gecersiz kullanici adi veya sifre.")
+    session["message"] = "Gecersiz kullanici adi veya sifre."
+    return redirect(url_for("index"))
 
 
 @app.route("/signup", methods=["POST"])
 def signup():
-    fullName = request.form["fullName"]
-    email = request.form["email"]
-    password = request.form["password"]
+    session["fullName"] = request.form["fullName"]
+    session["email"] = request.form["email"]
+    session["password"] = request.form["password"]
 
     users = create.selecALL()
 
     for user in users:
-        if user[2] == email:
-            return render_template("index.html", message="Bu eposta kayitli lutfen giris yapin!")
-    message = create.insert(fullName, email, password)
-    return render_template("index.html", message=message)
+        if user[2] == session.get("email"):
+           session["message"]="Bu eposta kayitli lutfen giris yapin!"
+           return redirect(url_for("index"))
+        
+    session["message"] = create.insert(session.get("fullName"), session.get("email"), session.get("password"))
+    
+    return redirect(url_for("index"))
 
 @app.route("/forgot")
 def forgot():
+    session["kod"] = random.randint(100000,999999)
+    session["email"] = request.form("email")
     return render_template("index.html")
 
 
